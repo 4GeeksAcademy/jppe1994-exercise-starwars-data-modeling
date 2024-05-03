af1b19d72afab22b79931baea000666288bd5811
Pedro Mendoza Full Stack DEV <157172448+jppe1994@users.noreply.github.com>
2024-05-03 17:20:11 +0000

    Diagrama corrregido

--- src/models.py
+++ src/models.py
@@ -27,6 +27,69 @@ class Address(Base):
 
     def to_dict(self):
         return {}
+    
+class Users(Base):
+    __tablename__ = 'users'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    lastName = Column(String(250), nullable=False)
+    mail = Column(String(250), nullable=False)
+
+class Personajes(Base):
+    __tablename__ = 'personajes'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+
+class Planetas(Base):
+    __tablename__ = 'planeta'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+
+    
+class propiedades_personajes(Base):
+    __tablename__ = 'pro_personaj'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    lastName = Column(String(250), nullable=False)
+    mail = Column(String(250), nullable=False)
+    personajes_id = Column(Integer, ForeignKey('personajes.id'))
+    personajes = relationship(Personajes)
+    planeta_id = Column(Integer, ForeignKey('planeta.id'))
+    planeta = relationship(Planetas)
+
+class Favoritos_personajes(Base):
+    __tablename__ = 'f_personajes'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    street_name = Column(String(250))
+    street_number = Column(String(250))
+    post_code = Column(String(250), nullable=False)
+    users_id = Column(Integer, ForeignKey('users.id'))
+    users = relationship(Users)
+    personajes_id = Column(Integer, ForeignKey('personajes.id'))
+    personajes = relationship(Personajes)
+
+class Favoritos_planetas(Base):
+    __tablename__ = 'f_planeta'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    users_id = Column(Integer, ForeignKey('users.id'))
+    users = relationship(Users)
+    planetas_id = Column(Integer, ForeignKey('planetas.id'))
+    planetas = relationship(Planetas)
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
